7047e3215a5f71aa0e410c163c9782539436730a
alona.barabash <alona.barabash@mail.ru>
2018-06-04 12:51:01 +0300

    Added LocationWeatherListViewModel and SearchLocationListViewModel. Deleted redundant ViewModels.

---
+++ SunnyWeatherApp/SunnyWeatherApp/ViewModels/LocationWeatherListViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+using SunnyWeatherApp.Models;
+using SunnyWeatherApp.Models.Location;
+using SunnyWeatherApp.Services.Abstractions;
+using SunnyWeatherApp.Views;
+
+namespace SunnyWeatherApp.ViewModels
+{
+    public class LocationWeatherListViewModel : BaseViewModel
+    {
+        private readonly ILocationSearchService _locationSearchServiceService;
+        private readonly IWeatherService _weatherService;
+
+        public LocationWeatherListViewModel(IWeatherService weatherService, ILocationSearchService locationSearchServiceService)
+        {
+            _weatherService = weatherService;
+            _locationSearchServiceService = locationSearchServiceService;
+            Title = "Browse weather";
+            LocationWeatherList = new ObservableCollection<LocationWeather>();
+            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+
+            MessagingCenter.Subscribe<SearchLocationListPage, Location>(this, "AddItem", async (obj, item) =>
+            {
+
+
+            });
+        }
+
+        public Command LoadItemsCommand { get; set; }
+        public ObservableCollection<LocationWeather> LocationWeatherList { get; set; }
+        async Task ExecuteLoadItemsCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                LocationWeatherList.Clear();
+                var locationList = await _locationSearchServiceService.GetItemListAsync(true);
+                foreach (var location in locationList)
+                {
+                    var weatherList = await _weatherService.GetCurrentWeatherByLocationAsync(location.Key);
+                    var locationWeather = new LocationWeather
+                    {
+                        Location = location,
+                        CurrentWeather = weatherList.FirstOrDefault()
+                    };
+
+                    LocationWeatherList.Add(locationWeather);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
---
+++ SunnyWeatherApp/SunnyWeatherApp/ViewModels/SearchLocationListViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using SunnyWeatherApp.Models;
+using SunnyWeatherApp.Models.Location;
+using SunnyWeatherApp.Services.Abstractions;
+using Xamarin.Forms;
+
+namespace SunnyWeatherApp.ViewModels
+{
+    public class SearchLocationListViewModel : BaseViewModel
+    {
+        private readonly ILocationSearchService _locationSearchServiceService;
+        private string _searchedText;
+
+        public SearchLocationListViewModel(ILocationSearchService locationSearchService)
+        {
+            _locationSearchServiceService = locationSearchService;
+            Title = "Browse";
+            LocationList = new ObservableCollection<Location>();
+            LoadItemsCommand = new Command(async () => await ExecuteSearchLocationListCommandAsync());
+        }
+
+        public ObservableCollection<Location> LocationList { get; set; }
+        public Command LoadItemsCommand { get; set; }
+
+        public string SearchedText
+        {
+            get => _searchedText;
+            set => SetProperty(ref _searchedText, value);
+        }
+
+        private async Task ExecuteSearchLocationListCommandAsync()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+            try
+            {
+
+                LocationList.Clear();
+                var locationList = await _locationSearchServiceService.GetLocationListByTextAsync(SearchedText);
+                foreach (var location in locationList)
+                {
+                    LocationList.Add(location);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
+
+
