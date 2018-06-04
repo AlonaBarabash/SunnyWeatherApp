715d929470ef210c0f88651555f8c595a85617ed
alona.barabash <alona.barabash@mail.ru>
2018-06-04 10:58:33 +0300

    Add project files.

---
+++ SunnyWeatherApp/SunnyWeatherApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using SunnyWeatherApp.Models;
+
+namespace SunnyWeatherApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
